53bfb3d095de3f48e88af30e305b1f83d61ece46
Mike Ramos <mramos@breproperties.com>
2011-08-17 17:39:09 -0700

    mocking up interface ideas

--- js/control.js
+++ js/control.js
@@ -11,14 +11,25 @@ $(document).ready(function() {
 
 			// Loop through each word in the response and format them
 			$(jsonResponse.words).each(function(key, val) {
-				$("section#words ul").append("<li><a href='#' class='" + val.priority + "' title='" + val.context + "'>" + val.item + "</a></li>\r\n");
+				$("section#words ul").append("<li><a href='#"+ val.item + "' class='" + val.priority + "' title='" + val.context + "'>" + val.item + "</a></li>\r\n");
+			});
+
+			$("li a").mouseover(function (event) {
+				//event.preventDefault();
+				//alert(event.type);
+				//jsonResponse.words[0].priority = "medium";
+
+				$("section#context").html(this.title);
+			});
+
+			$("li a").mouseout(function (event) {
+				//event.preventDefault();
+				$("section#context").html("");
 			});
 
 			$("li a").click(function (event) {
 				event.preventDefault();
-				//alert(event.type);
-				//alert(this.text);
-				jsonResponse.words[0].priority = "medium";
+				$(this).removeClass().addClass("medium");
 			});
 		}
 	});
